98871663fb4135cd7230b32b4066a2081dfe3c87
Hashir Hameed Niazi <hameedhashirniazi@gmail.com>
2020-08-02 08:55:46 +0530

    Configured Notifications from database

--- src/views/notification.vue
+++ src/views/notification.vue
@@ -1,10 +1,39 @@
 <template>
-  <h1 class="border-bottom">Heellow</h1>
+  <h1 class="border-bottom">Notifications</h1>
+
 </template>
 
 <script>
+import { db } from '../main'
 export default {
+  data: () => ({
+    notifications: []
+  }),
+  mounted() {
+    this.getItems()
+  },
+  methods: {
+    async getItems()
+    {
+      var itemsRef = db.ref('notification');
+      let notifications = []
+      itemsRef.on('value', function(snapshot){
+        for(const i in snapshot.val())
+        {
 
+          notifications.push({
+            id: i,
+            body: snapshot.val()[i].Body,
+            email: snapshot.val()[i].Email,
+            name: snapshot.val()[i].Name,
+            type: snapshot.val()[i].Type
+          })
+        }
+      })
+      console.log(notifications)
+      this.notifications = notifications
+    }
+  }
 }
 </script>
 
