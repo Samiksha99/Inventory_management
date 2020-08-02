7d152e89f823e3e8eb22db8f95d64fd39c68faa1
Hashir Hameed Niazi <hameedhashirniazi@gmail.com>
2020-08-02 07:49:07 +0530

    Added editable list frontend

--- src/components/singleItem.vue
+++ src/components/singleItem.vue
@@ -2,8 +2,15 @@
   <div :class="setAttribute(item.Total)">
   <div class="body">
     <h5 class="card-title">{{ item.Name }}</h5>
+    <div v-if="show">
     <p class="card-text">Number of items:- {{ item.Total }}</p>
-    <a href="#" :class="setButtonAttribute(item.Total)">Go somewhere</a>
+      <a @click="show = false"   :class="setButtonAttribute(item.Total)">Go somewhere</a>
+    </div>
+    <div v-else>
+      <input class="form-control" v-model="value" placeholder="Enter new value" />
+      <button @click="show = true" :class="setButtonAttribute(item.Total)"> Save </button>
+      <p class="text-muted"> This will notify other admins that the change is made. </p>
+    </div>
   </div>
 </div>
 </template>
@@ -11,6 +18,11 @@
 <script>
 export default {
     props: ['item'],
+    data: () => ({
+      value: 0,
+      absolute: true,
+      show: true
+    }),
     created() {
       console.log("Came Here")
     },
