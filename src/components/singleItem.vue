d6ae0c1dbcc6c4346e22b98812d2d231a6bc0b33
Hashir Hameed Niazi <hameedhashirniazi@gmail.com>
2020-08-02 07:28:32 +0530

    Update

--- src/components/singleItem.vue
+++ src/components/singleItem.vue
@@ -1,9 +1,9 @@
 <template>
   <div :class="setAttribute(item.Total)">
-  <div class="card-body">
+  <div class="body">
     <h5 class="card-title">{{ item.Name }}</h5>
     <p class="card-text">Number of items:- {{ item.Total }}</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <a href="#" :class="setButtonAttribute(item.Total)">Go somewhere</a>
   </div>
 </div>
 </template>
@@ -15,6 +15,21 @@ export default {
       console.log("Came Here")
     },
     methods: {
+      setButtonAttribute(number)
+      {
+        if(number>=0&&number<=4)
+        {
+          return 'btn btn-danger'
+        }
+        if(number>4&&number<=9)
+        {
+          return 'btn btn-primary'
+        }
+        if(number>9)
+        {
+          return 'btn btn-success'
+        }
+      },
       setAttribute(number)
       {
         if(number>=0&&number<=4)
@@ -36,12 +51,18 @@ export default {
 
 <style scoped>
   .danger {
-    border: solid red;
+    border: solid darkred;
+    color: darkred;
+    background-color: rgb(255, 184, 184);
   }
   .success {
-    border: solid greenyellow;
+    border: solid darkgreen;
+    color: darkgreen;
+    background-color: rgb(211, 255, 184);
   }
   .primary {
-    border: solid blue;
+    border: solid darkblue;
+    color: darkblue;
+    background-color: rgb(184, 255, 243);
   }
 </style>
